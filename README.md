b4ff655c0b3ece6a2450ea1f119f9b9285b15c69
MONSIUERBIR <74809010+MONSIUERBIR@users.noreply.github.com>
2023-12-25 02:01:57 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
-# AiGenerativebyBir
+---
+title: Stable Diffusion XL 0.9
+emoji: 🔥
+colorFrom: yellow
+colorTo: gray
+sdk: gradio
+sdk_version: 3.11.0
+app_file: app.py
+pinned: true
+license: mit
+---
+
+# StableDiffusion XL Gradio Demo
+This is a gradio demo supporting [Stable Diffusion XL 0.9](https://github.com/Stability-AI/generative-models). This demo loads the base and the refiner model.
+
+This is forked from [StableDiffusion v2.1 Demo](https://huggingface.co/spaces/gradio-client-demos/stable-diffusion). Refer to the git commits to see the changes.
+
+**Update:** Colab is supported! You can run this demo on Colab for free even on T4. <a target="_blank" href="https://colab.research.google.com/github/FurkanGozukara/Stable-Diffusion/blob/main/ColabNotebooks/Stable_Diffusion_SDXL_on_Google_Colab.ipynb">
+  <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
+</a>
+
+## Examples
+Left: SDXL 0.9. Right: [SD v2.1](https://huggingface.co/spaces/gradio-client-demos/stable-diffusion).
+
+Without any tuning, SDXL generates much better images compared to SD v2.1!
+
+### Example 1
+<p align="middle">
+<img src="imgs/img1_sdxl0.9.png" width="48%">
+<img src="imgs/img1_sdv2.1.png" width="48%">
+</p>
+
+### Example 2
+<p align="middle">
+<img src="imgs/img2_sdxl0.9.png" width="48%">
+<img src="imgs/img2_sdv2.1.png" width="48%">
+</p>
+
+### Example 3
+<p align="middle">
+<img src="imgs/img3_sdxl0.9.png" width="48%">
+<img src="imgs/img3_sdv2.1.png" width="48%">
+</p>
+
+### Example 4
+<p align="middle">
+<img src="imgs/img4_sdxl0.9.png" width="48%">
+<img src="imgs/img4_sdv2.1.png" width="48%">
+</p>
+
+### Example 5
+<p align="middle">
+<img src="imgs/img5_sdxl0.9.png" width="48%">
+<img src="imgs/img5_sdv2.1.png" width="48%">
+</p>
+
+## Installation
+With torch 2.0.1 installed, we also need to install:
+```shell
+pip install accelerate transformers invisible-watermark "numpy>=1.17" "PyWavelets>=1.1.1" "opencv-python>=4.1.0.25" safetensors "gradio==3.11.0"
+pip install git+https://github.com/huggingface/diffusers.git@sd_xl
+```
+
+## Launching
+It's free but you need to [submit a quick form](https://huggingface.co/stabilityai/stable-diffusion-xl-base-0.9) to get access to the weights.
+
+There are two ways to load the weights. After getting access to weights, you can either clone them locally or this repo can load them for you.
+
+### Option 1
+If you have cloned both repo ([base](https://huggingface.co/stabilityai/stable-diffusion-xl-base-0.9), [refiner](https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-0.9)) locally (please change the `path_to_sdxl`):
+```
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:512 SDXL_MODEL_DIR=/path_to_sdxl python app.py
+```
+
+### Option 2
+If you want to load from the huggingface hub (please set up a [HuggingFace access token](https://huggingface.co/docs/hub/security-tokens)):
+```
+PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:512 ACCESS_TOKEN=YOUR_HF_ACCESS_TOKEN python app.py
+```
+
+### `torch.compile` support
+Turn on `torch.compile` will make overall inference faster. However, this will add some overhead to the first run (i.e., have to wait for compilation during the first run).
+
+### To save memory
+1. Turn on `pipe.enable_model_cpu_offload()` and turn off `pipe.to("cuda")` in `app.py`.
+2. Turn off refiner by setting `enable_refiner` to False.
+3. More ways to [save memory and make things faster](https://huggingface.co/docs/diffusers/optimization/fp16).
+
+### Several options through environment variables
+* `SDXL_MODEL_DIR` and `ACCESS_TOKEN`: load SDXL locally or from HF hub.
+* `ENABLE_REFINER=true/false` turn on/off the refiner ([refiner](https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-0.9) refines the generation).
+* `OUTPUT_IMAGES_BEFORE_REFINER=true/false` useful is refiner is enabled. Output images before and after the refiner stage.
+* `SHARE=true/false` creates public link (useful for sharing and on colab)
+
+## If you enjoy this demo, please give [this repo](https://github.com/TonyLianLong/stable-diffusion-xl-demo) a star ⭐.
---
+++ app5.py
@@ -0,0 +1,444 @@
+import gradio as gr
+
+from diffusers import DiffusionPipeline
+import torch
+
+import base64
+from io import BytesIO
+import os
+import gc
+from datetime import datetime
+
+from share_btn import community_icon_html, loading_icon_html, share_js
+
+# SDXL code: https://github.com/huggingface/diffusers/pull/3859
+
+model_dir = os.getenv("SDXL_MODEL_DIR")
+access_token = os.getenv("ACCESS_TOKEN")
+
+if model_dir:
+    # Use local model
+    model_key_base = os.path.join(model_dir, "stable-diffusion-xl-base-0.9")
+    model_key_refiner = os.path.join(model_dir, "stable-diffusion-xl-refiner-0.9")
+else:
+    model_key_base = "OwlMaster/gg"
+    model_key_refiner = "OwlMaster/gg2"
+
+# Use refiner (enabled by default)
+enable_refiner = os.getenv("ENABLE_REFINER", "true").lower() == "true"
+# Output images before the refiner and after the refiner
+output_images_before_refiner = os.getenv("OUTPUT_IMAGES_BEFORE_REFINER", "false").lower() == "true"
+
+# Create public link
+share = os.getenv("SHARE", "false").lower() == "true"
+
+print("Loading model", model_key_base)
+pipe = DiffusionPipeline.from_pretrained(model_key_base, torch_dtype=torch.float16, use_auth_token=access_token)
+
+#pipe.enable_model_cpu_offload()
+pipe.to("cuda")
+
+# if using torch < 2.0
+pipe.enable_xformers_memory_efficient_attention()
+
+
+
+# pipe.unet = torch.compile(pipe.unet, mode="reduce-overhead", fullgraph=True)
+
+if enable_refiner:
+    print("Loading model", model_key_refiner)
+    pipe_refiner = DiffusionPipeline.from_pretrained(model_key_base, torch_dtype=torch.float16, use_auth_token=access_token)
+    #pipe_refiner.enable_model_cpu_offload()
+    pipe_refiner.to("cuda")
+
+    # if using torch < 2.0
+    pipe_refiner.enable_xformers_memory_efficient_attention()
+
+    # pipe_refiner.unet = torch.compile(pipe_refiner.unet, mode="reduce-overhead", fullgraph=True)
+
+# NOTE: we do not have word list filtering in this gradio demo
+
+
+
+is_gpu_busy = False
+def infer(prompt, negative, scale, samples=1, steps=50, refiner_steps=15, num_images=1):
+    prompt, negative = [prompt] * samples, [negative] * samples
+    images_b64_list = []
+    for i in range(0, num_images):
+        images = pipe(prompt=prompt, negative_prompt=negative, guidance_scale=scale, num_inference_steps=steps).images
+        os.makedirs(r"stable-diffusion-xl-demo/outputs", exist_ok=True)
+        gc.collect()
+        torch.cuda.empty_cache()
+
+        if enable_refiner:
+            if output_images_before_refiner:
+                for image in images:
+                    buffered = BytesIO()
+                    image.save(buffered, format="JPEG")
+                    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+                    image_b64 = (f"data:image/jpeg;base64,{img_str}")
+                    images_b64_list.append(image_b64)
+                    # Save the image as PNG with unique timestamp
+                    filename = f"stable-diffusion-xl-demo/outputs/generated_image_{timestamp}_{i}.png"
+                    image.save(filename, format="PNG")
+
+            images = pipe_refiner(prompt=prompt, negative_prompt=negative, num_inference_steps=refiner_steps).images
+
+            gc.collect()
+            torch.cuda.empty_cache()
+
+        # Create the outputs folder if it doesn't exist
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+
+        for i, image in enumerate(images):
+            buffered = BytesIO()
+            image.save(buffered, format="JPEG")
+            img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+            image_b64 = (f"data:image/jpeg;base64,{img_str}")
+            images_b64_list.append(image_b64)
+            # Save the image as PNG with unique timestamp
+            filename = f"stable-diffusion-xl-demo/outputs/generated_image_{timestamp}_{i}.png"
+            image.save(filename, format="PNG")
+
+    return images_b64_list
+
+    
+    
+css = """
+        .gradio-container {
+            font-family: 'IBM Plex Sans', sans-serif;
+        }
+        .gr-button {
+            color: white;
+            border-color: black;
+            background: black;
+        }
+        input[type='range'] {
+            accent-color: black;
+        }
+        .dark input[type='range'] {
+            accent-color: #dfdfdf;
+        }
+        .container {
+            max-width: 730px;
+            margin: auto;
+            padding-top: 1.5rem;
+        }
+        #gallery {
+            min-height: 22rem;
+            margin-bottom: 15px;
+            margin-left: auto;
+            margin-right: auto;
+            border-bottom-right-radius: .5rem !important;
+            border-bottom-left-radius: .5rem !important;
+        }
+        #gallery>div>.h-full {
+            min-height: 20rem;
+        }
+        .details:hover {
+            text-decoration: underline;
+        }
+        .gr-button {
+            white-space: nowrap;
+        }
+        .gr-button:focus {
+            border-color: rgb(147 197 253 / var(--tw-border-opacity));
+            outline: none;
+            box-shadow: var(--tw-ring-offset-shadow), var(--tw-ring-shadow), var(--tw-shadow, 0 0 #0000);
+            --tw-border-opacity: 1;
+            --tw-ring-offset-shadow: var(--tw-ring-inset) 0 0 0 var(--tw-ring-offset-width) var(--tw-ring-offset-color);
+            --tw-ring-shadow: var(--tw-ring-inset) 0 0 0 calc(3px var(--tw-ring-offset-width)) var(--tw-ring-color);
+            --tw-ring-color: rgb(191 219 254 / var(--tw-ring-opacity));
+            --tw-ring-opacity: .5;
+        }
+        #advanced-btn {
+            font-size: .7rem !important;
+            line-height: 19px;
+            margin-top: 12px;
+            margin-bottom: 12px;
+            padding: 2px 8px;
+            border-radius: 14px !important;
+        }
+        #advanced-options {
+            display: none;
+            margin-bottom: 20px;
+        }
+        .footer {
+            margin-bottom: 45px;
+            margin-top: 35px;
+            text-align: center;
+            border-bottom: 1px solid #e5e5e5;
+        }
+        .footer>p {
+            font-size: .8rem;
+            display: inline-block;
+            padding: 0 10px;
+            transform: translateY(10px);
+            background: white;
+        }
+        .dark .footer {
+            border-color: #303030;
+        }
+        .dark .footer>p {
+            background: #0b0f19;
+        }
+        .acknowledgments h4{
+            margin: 1.25em 0 .25em 0;
+            font-weight: bold;
+            font-size: 115%;
+        }
+        .animate-spin {
+            animation: spin 1s linear infinite;
+        }
+        @keyframes spin {
+            from {
+                transform: rotate(0deg);
+            }
+            to {
+                transform: rotate(360deg);
+            }
+        }
+        #share-btn-container {
+            display: flex; padding-left: 0.5rem !important; padding-right: 0.5rem !important; background-color: #000000; justify-content: center; align-items: center; border-radius: 9999px !important; width: 13rem;
+            margin-top: 10px;
+            margin-left: auto;
+        }
+        #share-btn {
+            all: initial; color: #ffffff;font-weight: 600; cursor:pointer; font-family: 'IBM Plex Sans', sans-serif; margin-left: 0.5rem !important; padding-top: 0.25rem !important; padding-bottom: 0.25rem !important;right:0;
+        }
+        #share-btn * {
+            all: unset;
+        }
+        #share-btn-container div:nth-child(-n+2){
+            width: auto !important;
+            min-height: 0px !important;
+        }
+        #share-btn-container .wrap {
+            display: none !important;
+        }
+        
+        .gr-form{
+            flex: 1 1 50%; border-top-right-radius: 0; border-bottom-right-radius: 0;
+        }
+        #prompt-container{
+            gap: 0;
+        }
+        #prompt-text-input, #negative-prompt-text-input{padding: .45rem 0.625rem}
+        #component-16{border-top-width: 1px!important;margin-top: 1em}
+        .image_duplication{position: absolute; width: 100px; left: 50px}
+"""
+
+block = gr.Blocks(css=css, timeout=300)
+
+examples = [
+    [
+        'A high tech solarpunk utopia in the Amazon rainforest',
+        'low quality',
+        9
+    ],
+    [
+        'A pikachu fine dining with a view to the Eiffel Tower',
+        'low quality',
+        9
+    ],
+    [
+        'A mecha robot in a favela in expressionist style',
+        'low quality, 3d, photorealistic',
+        9
+    ],
+    [
+        'an insect robot preparing a delicious meal',
+        'low quality, illustration',
+        9
+    ],
+    [
+        "A small cabin on top of a snowy mountain in the style of Disney, artstation",
+        'low quality, ugly',
+        9
+    ],
+]
+
+
+with block:
+    gr.HTML(
+        """
+            <div style="text-align: center; margin: 0 auto;">
+              <div
+                style="
+                  display: inline-flex;
+                  align-items: center;
+                  gap: 0.8rem;
+                  font-size: 1.75rem;
+                "
+              >
+                <svg
+                  width="0.65em"
+                  height="0.65em"
+                  viewBox="0 0 115 115"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect width="23" height="23" fill="white"></rect>
+                  <rect y="69" width="23" height="23" fill="white"></rect>
+                  <rect x="23" width="23" height="23" fill="#AEAEAE"></rect>
+                  <rect x="23" y="69" width="23" height="23" fill="#AEAEAE"></rect>
+                  <rect x="46" width="23" height="23" fill="white"></rect>
+                  <rect x="46" y="69" width="23" height="23" fill="white"></rect>
+                  <rect x="69" width="23" height="23" fill="black"></rect>
+                  <rect x="69" y="69" width="23" height="23" fill="black"></rect>
+                  <rect x="92" width="23" height="23" fill="#D9D9D9"></rect>
+                  <rect x="92" y="69" width="23" height="23" fill="#AEAEAE"></rect>
+                  <rect x="115" y="46" width="23" height="23" fill="white"></rect>
+                  <rect x="115" y="115" width="23" height="23" fill="white"></rect>
+                  <rect x="115" y="69" width="23" height="23" fill="#D9D9D9"></rect>
+                  <rect x="92" y="46" width="23" height="23" fill="#AEAEAE"></rect>
+                  <rect x="92" y="115" width="23" height="23" fill="#AEAEAE"></rect>
+                  <rect x="92" y="69" width="23" height="23" fill="white"></rect>
+                  <rect x="69" y="46" width="23" height="23" fill="white"></rect>
+                  <rect x="69" y="115" width="23" height="23" fill="white"></rect>
+                  <rect x="69" y="69" width="23" height="23" fill="#D9D9D9"></rect>
+                  <rect x="46" y="46" width="23" height="23" fill="black"></rect>
+                  <rect x="46" y="115" width="23" height="23" fill="black"></rect>
+                  <rect x="46" y="69" width="23" height="23" fill="black"></rect>
+                  <rect x="23" y="46" width="23" height="23" fill="#D9D9D9"></rect>
+                  <rect x="23" y="115" width="23" height="23" fill="#AEAEAE"></rect>
+                  <rect x="23" y="69" width="23" height="23" fill="black"></rect>
+                </svg>
+                <h1 style="font-weight: 900; margin-bottom: 7px;margin-top:5px">
+                  Stable Diffusion XL 0.9 Demo
+                </h1>
+              </div>
+              <p style="margin-bottom: 10px; font-size: 94%; line-height: 23px;">
+			    Brought you by SECourses : <a style="text-decoration: underline;" href="https://www.youtube.com/SECourses">https://www.youtube.com/SECourses</a>
+				<br/>
+				Please support me on Patreon : <a style="text-decoration: underline;" href="https://www.patreon.com/SECourses">https://www.patreon.com/SECourses</a>
+				<br/>
+				Patreon exclusive posts index : <a style="text-decoration: underline;" href="https://github.com/FurkanGozukara/Stable-Diffusion/blob/main/Patreon-Posts-Index.md">https://github.com/FurkanGozukara/Stable-Diffusion/blob/main/Patreon-Posts-Index.md</a>
+				<br/>
+                Stable Diffusion XL 0.9 is the latest text-to-image model from StabilityAI. 
+                <br/>
+                For faster generation and API access you can try
+                <a
+                  href="http://beta.dreamstudio.ai/"
+                  style="text-decoration: underline;"
+                  target="_blank"
+                  >DreamStudio Beta</a
+                >.</a>
+              </p>
+            </div>
+        """
+    )
+    with gr.Group():
+        with gr.Box():
+            with gr.Row(elem_id="prompt-container").style(mobile_collapse=False, equal_height=True):
+                with gr.Column():
+                    text = gr.Textbox(
+                        label="Enter your prompt",
+                        show_label=False,
+                        max_lines=1,
+                        placeholder="Enter your prompt",
+                        elem_id="prompt-text-input",
+                    ).style(
+                        border=(True, False, True, True),
+                        rounded=(True, False, False, True),
+                        container=False,
+                    )
+                    negative = gr.Textbox(
+                        label="Enter your negative prompt",
+                        show_label=False,
+                        max_lines=1,
+                        placeholder="Enter a negative prompt",
+                        elem_id="negative-prompt-text-input",
+                    ).style(
+                        border=(True, False, True, True),
+                        rounded=(True, False, False, True),
+                        container=False,
+                    )
+                btn = gr.Button("Generate image").style(
+                    margin=False,
+                    rounded=(False, True, True, False),
+                    full_width=False,
+                )
+                num_images = gr.Slider(
+                    label="Number of Images",
+                    minimum=1,
+                    maximum=1000,
+                    value=1,
+                    step=1,
+					enabled=True
+                )
+
+        gallery = gr.Gallery(
+            label="Generated images", show_label=False, elem_id="gallery"
+        ).style(grid=[2], height="auto")
+
+        with gr.Group(elem_id="container-advanced-btns"):
+            #advanced_button = gr.Button("Advanced options", elem_id="advanced-btn")
+            with gr.Group(elem_id="share-btn-container"):
+                community_icon = gr.HTML(community_icon_html)
+                loading_icon = gr.HTML(loading_icon_html)
+                share_button = gr.Button("Share to community", elem_id="share-btn")
+
+        with gr.Accordion("Advanced settings", open=False):
+        #    gr.Markdown("Advanced settings are temporarily unavailable")
+            samples = gr.Slider(label="Images", minimum=1, maximum=8, value=1, step=1)
+            steps = gr.Slider(label="Steps", minimum=1, maximum=250, value=50, step=1)
+            if enable_refiner:
+                refiner_steps = gr.Slider(label="Refiner Steps", minimum=1, maximum=50, value=15, step=1)
+            else:
+                refiner_steps = gr.Slider(label="Refiner Steps (refiner not enabled)", minimum=0, maximum=0, value=0, step=1)
+            guidance_scale = gr.Slider(
+                label="Guidance Scale", minimum=0, maximum=50, value=9, step=0.1
+            )
+        #    seed = gr.Slider(
+        #        label="Seed",
+        #        minimum=0,
+        #        maximum=2147483647,
+        #        step=1,
+        #        randomize=True,
+        #    )
+
+        ex = gr.Examples(examples=examples, fn=infer, inputs=[text, negative, guidance_scale], outputs=[gallery, community_icon, loading_icon, share_button], cache_examples=False)
+        ex.dataset.headers = [""]
+        negative.submit(infer, inputs=[text, negative, guidance_scale, samples, steps, refiner_steps, num_images], outputs=[gallery], postprocess=False)
+        text.submit(infer, inputs=[text, negative, guidance_scale, samples, steps, refiner_steps, num_images], outputs=[gallery], postprocess=False)
+        btn.click(infer, inputs=[text, negative, guidance_scale, samples, steps, refiner_steps, num_images], outputs=[gallery], postprocess=False)
+        
+        #advanced_button.click(
+        #    None,
+        #    [],
+        #    text,
+        #    _js="""
+        #    () => {
+        #        const options = document.querySelector("body > gradio-app").querySelector("#advanced-options");
+        #        options.style.display = ["none", ""].includes(options.style.display) ? "flex" : "none";
+        #    }""",
+        #)
+        share_button.click(
+            None,
+            [],
+            [],
+            _js=share_js,
+        )
+        gr.HTML(
+            """
+                <div class="footer">
+                    <p>Model by <a href="https://huggingface.co/stabilityai" style="text-decoration: underline;" target="_blank">StabilityAI</a> - Gradio Demo by 🤗 Hugging Face and <a style="text-decoration: underline;" href="https://tonylian.com/">Long (Tony) Lian</a> & <a style="text-decoration: underline;" href="https://www.linkedin.com/in/furkangozukara/">Furkan Gözükara</a>
+                    </p>
+                </div>
+           """
+        )
+        with gr.Accordion(label="License", open=False):
+            gr.HTML(
+                """<div class="acknowledgments">
+                    <p><h4>LICENSE</h4>
+The model is licensed with a <a href="https://huggingface.co/stabilityai/stable-diffusion-2/blob/main/LICENSE-MODEL" style="text-decoration: underline;" target="_blank">CreativeML OpenRAIL++</a> license. The authors claim no rights on the outputs you generate, you are free to use them and are accountable for their use which must not go against the provisions set in this license. The license forbids you from sharing any content that violates any laws, produce any harm to a person, disseminate any personal information that would be meant for harm, spread misinformation and target vulnerable groups. For the full list of restrictions please <a href="https://huggingface.co/spaces/CompVis/stable-diffusion-license" target="_blank" style="text-decoration: underline;" target="_blank">read the license</a></p>
+                    <p><h4>Biases and content acknowledgment</h4>
+Despite how impressive being able to turn text into image is, beware to the fact that this model may output content that reinforces or exacerbates societal biases, as well as realistic faces, pornography and violence. The model was trained on the <a href="https://laion.ai/blog/laion-5b/" style="text-decoration: underline;" target="_blank">LAION-5B dataset</a>, which scraped non-curated image-text-pairs from the internet (the exception being the removal of illegal content) and is meant for research purposes. You can read more in the <a href="https://huggingface.co/CompVis/stable-diffusion-v1-4" style="text-decoration: underline;" target="_blank">model card</a></p>
+               </div>
+                """
+            )
+
+block.queue().launch(share=share)
